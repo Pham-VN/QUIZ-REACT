8c4452e83078e8d62b6341d98bcb2d75688caaed
Pham-VN <thatnhitam@gmail.com>
2023-09-26 16:00:33 +0200

    Réussir d'aficher chaque question, en utilisant le componant et hook useState

---
+++ Desktop/Quiz_React/quiz-app/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import { Fragment } from 'react';
+import DisplayQuiz from './components/displayQuestionPhraseAndAnswers.js'
+
+function App() {
+  return (
+    <Fragment>
+      < DisplayQuiz />
+      </Fragment>
+  );
+}
+
+export default App;
